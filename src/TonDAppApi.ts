a0f1d3d3ca63f18683587d5d8ba7a5ef42595770
Andrey T <a.a.tereschenko@gmail.com>
2024-06-01 21:49:44 +0200

    updated url

--- src/TonDAppApi.ts
+++ src/TonDAppApi.ts
@@ -1,7 +1,7 @@
 import { AddressDto, Addresses, CollectionDetailsDto, NFTCollectionListDto, NFTItemDescriptionDto, NFTItemsListDto } from "./server/dto/collection-dto";
 
-const collectionUrl = 'https://testnet.toncenter.com/api/v3/nft/collections?collection_address=EQBrcWXXk5h4a-xcYCJC6tHiS2eJlox6o60HJzzyrdZ6vQwB&owner_address=EQD9votaYZ0pI-YfIweCtIgK1263P6CJH8cEfBqEWMZTucKk&limit=128&offset=0';
-const itemsUrl = 'https://testnet.toncenter.com/api/v3/nft/items?collection_address=EQBrcWXXk5h4a-xcYCJC6tHiS2eJlox6o60HJzzyrdZ6vQwB&limit=128&offset=0';
+const collectionUrl = 'https://testnet.toncenter.com/api/v3/nft/collections?collection_address=EQD1GrBkR0HEExqdx1LKvueMqzS35zqmcvqnnyJfhKlDPn34&owner_address=EQD9votaYZ0pI-YfIweCtIgK1263P6CJH8cEfBqEWMZTucKk&limit=128&offset=0';
+const itemsUrl = 'https://testnet.toncenter.com/api/v3/nft/items?collection_address=EQD1GrBkR0HEExqdx1LKvueMqzS35zqmcvqnnyJfhKlDPn34&limit=128&offset=0';
 const testAddressBaseURL = 'https://665b762e3e4ac90a04d74863.mockapi.io/api/v1/addresses'
 class TonDAppApiService {
   private collectionUrl = collectionUrl;
